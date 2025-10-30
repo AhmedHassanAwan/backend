25d3ec2eb0c967ac9b3a75ac3ad8beff34c0401f
Ahmed Hassan Awan <awanhasan2007@gmail.com>
2025-10-31 01:15:30 +0500

    serveradded

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ const app = express();
 app.use(cors({
   origin: [
     "http://localhost:5174", 
+    "http://localhost:5173",
     "https://frontend-expense-chi.vercel.app"
   ],
   credentials: true
